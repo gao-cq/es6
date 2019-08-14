0bc8bf38ff8b7a384a46cba72a9d32743a458a45
snow <snow@163.com>
2019-08-14 20:40:55 +0800

    排序学习

--- Array.prototype.sort.js
+++ Array.prototype.sort.js
@@ -1,41 +1,155 @@
-// var arr = [1, 3, 5, 2, 4, 9, 6, -1, 0]
-
-// Array.prototype.sort = function(fun, num = this.length, l = 0) {
-// 	while (++l < num) {
-// 		if (fun(this[l], this[l - 1]) < 0) {
-// 			this[l] = this[l] + this[l - 1] - (this[l - 1] = this[l])
-// 			this.sort(fun, l)
-// 		}
-// 	}
-// 	return this
-// }
-
-// function sortnum(a, b) {
-// 	// return a - b //递增
-// 	return b - a //递减
-// }
-
-// console.log(arr.sort(sortnum))
-
-
-var arr = [1, 3, 5, 2, 4, 9, 6, -1, 0, 7]
-function quickSort(arr) {
-    if(arr.length == 0) {
-        return [];  // 返回空数组
-    }
-    var cIndex = Math.floor(arr.length / 2);
-    var c = arr.splice(cIndex, 1);
-    var l = [];
-    var r = [];
-
-    for (var i = 0; i < arr.length; i++) {
-        if(arr[i] < c) {
-            l.push(arr[i]);
-        } else {
-            r.push(arr[i]);
-        }
-    }
-    return quickSort(l).concat(c, quickSort(r));
+var arr=[],t=0,num=100000
+console.time("数组生成时间");
+for(var i=0;i<num;i++){
+    arr.push( Math.floor( Math.random()*num*10 ) )
 }
-console.log(quickSort(arr));
-//[1, 2, 3, 4, 5, 6, 7]
+console.timeEnd("数组生成时间");
+
+
+Array.prototype.sort = function( fun ) {
+    //交换
+    function swap(arr, x, y){
+        var temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+    }
+
+    //冒泡排序
+    function maopaoSort(arr){
+        for(var i = 0; i < arr.length; i++){
+            for(var j = 0; j<arr.length; j++){
+                if( fun(arr[j], arr[i])<0 ){
+                    swap(arr, i, j)
+                }
+            }
+        }
+        return arr
+    }
+
+    //选择排序
+    function selectSort(arr){
+        for(var i = 0; i < arr.length; i++){
+            for(var j=i+1; j<arr.length; j++){
+                if( fun(arr[j], arr[i])<0 ){
+                    swap(arr, i, j)
+                }
+            }
+        }
+        return arr
+    }
+
+    //插入排序
+    function insertionSort(arr){
+        for (var i = 1; i < arr.length; i++) {
+            var j=i-1;
+            while (--j>-1 && fun(arr[j+1], arr[j]) < 0) {
+                swap(arr, j+1, j)
+            }
+        }
+        return arr
+    }
+
+    //希尔排序  插入排序的优化
+    function shellSort(arr){
+        var j =Math.floor(arr.length/2)
+        while(j>0){
+            for(var a=0;a<j;a++){
+                for (var i = a; i < arr.length; i+=j) {
+                    for(var k = i; k > j-1; k-=j){
+                        if(fun(arr[k],arr[k-j])<0){
+                            swap(arr, k, k-j)
+                        }else{
+                            break
+                        }
+                    }
+                }
+            }
+            j=Math.floor(j/2)
+        }
+        return arr
+    }
+
+    //快速排序
+    function quickSort(arr) {
+        if(arr.length <2 ) {
+            return arr
+        }
+        var c = arr[0]
+        var l = [];
+        var r = [];
+        for (var i = 1; i < arr.length; i++) {
+            if(fun(arr[i], c)<0) {
+                l.push(arr[i]);
+            } else {
+                r.push(arr[i]);
+            }
+        }
+        return quickSort(l).concat(c, quickSort(r));
+    }
+
+    //归并排序
+    function mergeSort(arr){
+        function subSort(arr1,arr2){
+            var arr=[],arr1=arr1.slice(),arr2=arr2.slice()
+            while(arr1.length || arr2.length){
+                if(arr1.length==0){
+                    arr = arr.concat( arr2 )
+                    arr2=[]
+                }
+                else if(arr2.length==0){
+                    arr = arr.concat( arr1 )
+                    arr1=[]
+                }
+                else if( fun(arr1[0], arr2[0])<0 ){
+                    arr.push( arr1.shift() )
+                }else{
+                    arr.push( arr2.shift() )
+                }
+            }
+            return arr
+        }
+        var len = arr.length,i=1,larr=[];
+        while(i < len){
+            larr.push(i)
+            i=i*2
+        }
+        for(var i=0; i<larr.length; i++){
+            var add=larr[i]
+            for(var j=0;j<arr.length;j+=add*2){
+                arr = arr.slice(0,j).concat( subSort(arr.slice(j,j+add), arr.slice(j+add,j+add*2) ), arr.slice(j+add*2) )
+            }
+        }
+        return arr
+
+    }
+
+    // return maopaoSort(this)
+    // return selectSort(this)
+    // return insertionSort(this)
+    return shellSort(this)
+    return quickSort(this)
+    return mergeSort(this)
+
+}
+
+
+
+function sortnum(a, b) {
+    t++
+	// return a - b //递增
+	return b - a //递减
+}
+
+console.time("排序时间");
+
+var newArr= arr.sort(sortnum)
+
+console.timeEnd("排序时间");
+
+console.log("比较次数："+ t )
+
+console.log( "排序后：" )
+console.log( newArr.slice(0,5) )
+console.log( newArr.slice(newArr.length-5,newArr.length) )
+
+
--- xh.js
+++ xh.js
@@ -1,3 +1,76 @@
-for(var i=1;i<10;i++){
-	console.log(i)
+// for(var i=1;i<10;i++){
+// 	console.log(i)
+// }
+
+
+for(var i=0,sum=0;i<=100;i++){
+	if(i%2==0){
+		sum+=i
+	}
 }
+
+console.log( sum )
+
+
+
+var i=0,sum=0;
+do{
+	if(i%2==0){
+		sum+=i
+	}
+	i++
+}while(i<=100)
+
+console.log( sum )
+
+// var i , j;
+// for(i = 1;i <= 9;i ++){
+//     document.write("<br>");       
+//     for(j = 1;j <= i;j ++){
+//         sum = i * j;
+//         document.write(i ,"*",j ,"=",sum,"   ");    
+//     }
+// }
+
+var arr=[];
+for(var i=0;i<20;i++){
+	arr.push(Math.floor(Math.random()*51)+150)
+}
+
+// function quickSort(arr) {
+//     if(arr.length == 0) {
+//         return [];  // 返回空数组
+//     }
+//     var cIndex = Math.floor(arr.length / 2);
+//     var c = arr.splice(cIndex, 1);
+//     var l = [];
+//     var r = [];
+
+//     for (var i = 0; i < arr.length; i++) {
+//         if(arr[i] > c) {
+//             l.push(arr[i]);
+//         } else {
+//             r.push(arr[i]);
+//         }
+//     }
+//     return quickSort(l).concat(c, quickSort(r));
+// }
+// console.log(quickSort(arr));
+
+
+Array.prototype.sort = function(fun, num = this.length, l = 0) {
+	while (++l < num) {
+		if (fun(this[l], this[l - 1]) < 0) {
+			this[l] = this[l] + this[l - 1] - (this[l - 1] = this[l])
+			this.sort(fun, l)
+		}
+	}
+	return this
+}
+
+function sortnum(a, b) {
+	// return a - b //递增
+	return b - a //递减
+}
+
+console.log(arr.sort(sortnum))
